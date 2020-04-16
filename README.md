738fcde05414ef448498c922fc1bfbc40f5de931
summernian <315652758@qq.com>
2020-04-16 14:44:31 +0800

    级联选择器

---
+++ README.md
@@ -0,0 +1,4 @@
+# -
+级联选择器（zepto、picker.js）
+
+预览地址 [点击这里](https://summernian.github.io/BaiduMap-findHouse/)
---
+++ js/cityData.js
@@ -0,0 +1,199 @@
+var picker_data = [{
+	name: '安徽',
+	value: 'AnHui',
+	sub: [{
+		name: '合肥',
+		value: 'HeFei',
+		sub: [{
+			name: '蜀山区',
+			value: 'ShuShanQu'
+		},{
+			name: '包河区',
+			value: 'BaoHeQu'
+		},{
+			name: '庐阳区',
+			value: 'LuYangQu'
+		},{
+			name: '瑶海区',
+			value: 'YaoHaiQu'
+		}]
+	},{
+		name: '六安',
+		value: 'luAn',
+		sub: [{
+			name: '金安区',
+			value: 'JinAnQu'
+		},{
+			name: '裕安区',
+			value: 'YuAnQu'
+		},{
+			name: '叶集区',
+			value: 'YeJiQu'
+		}]
+	},{
+		name: '芜湖',
+		value: 'WuHu',
+		sub: [{
+			name: '镜湖区',
+			value: 'JingHuQu'
+		},{
+			name: '鸠江区',
+			value: 'JiuJiangQu'
+		},{
+			name: '弋江区',
+			value: 'YeJiangQu'
+		},{
+			name: '三山区',
+			value: 'SanShanQu'
+		}]
+	}]
+},{
+	name: '江苏',
+	value: 'JiangSu',
+	sub: [{
+		name: '南京',
+		value: 'NanJing',
+		sub: [{
+			name: '玄武区',
+			value: 'XuanWuQu'
+		},{
+			name: '秦淮区',
+			value: 'QinHuaiQu'
+		},{
+			name: '鼓楼区',
+			value: 'GuLouQU'
+		},{
+			name: '建邺区',
+			value: 'JianYeQu'
+		},{
+			name: '雨花台区',
+			value: 'YuHuaTaiQu'
+		},{
+			name: '栖霞区',
+			value: 'QiXiaQu'
+		},{
+			name: '浦口区',
+			value: 'PuKouQu'
+		},{
+			name: '六合区',
+			value: 'LiuHeQu'
+		},{
+			name: '江宁区',
+			value: 'JiangNingQu'
+		},{
+			name: '溧水区',
+			value: 'LiShuiQu'
+		},{
+			name: '高淳区',
+			value: 'GaoChunQu'
+		}]
+	},{
+		name: '苏州',
+		value: 'SuiZhou',
+		sub: [{
+			name: '姑苏区',
+			value: 'GuSuQu'
+		},{
+			name: '相城区',
+			value: 'XiangChengQu'
+		},{
+			name: '吴中区',
+			value: 'WuZhongQu'
+		},{
+			name: '虎丘区',
+			value: 'HuQiuQu'
+		},{
+			name: '吴江区',
+			value: 'WuJiangQu'
+		}]
+	},{
+		name: '常州',
+		value: 'ChangZhou',
+		sub: [{
+			name: '天宁区',
+			value: 'TianNingQu'
+		},{
+			name: '钟楼区',
+			value: 'ZhongLouQu'
+		},{
+			name: '新北区',
+			value: 'XinBeiQu'
+		},{
+			name: '武进区',
+			value: 'WuJinQu'
+		},{
+			name: '金坛区',
+			value: 'JinTanQu'
+		}]
+	}]
+},{
+	name: '浙江',
+	value: 'ZheJiang',
+	sub: [{
+		name: '杭州',
+		value: 'HangZhou',
+		sub: [{
+			name: '上城区',
+			value: 'ShangChengQu'
+		},{
+			name: '下城区',
+			value: 'XiaChengQu'
+		},{
+			name: '江干区',
+			value: 'JiangGanQu'
+		},{
+			name: '拱墅区',
+			value: 'GongShuQu'
+		},{
+			name: '西湖区',
+			value: 'XiHuQu'
+		},{
+			name: '滨江区',
+			value: 'BinJiangQu'
+		},{
+			name: '萧山区',
+			value: 'XiaoShanQu'
+		},{
+			name: '余杭区',
+			value: 'YuHangQu'
+		},{
+			name: '富阳区',
+			value: 'FuYangQu'
+		},{
+			name: '临安区',
+			value: 'LinAnQu'
+		}]
+	},{
+		name: '宁波',
+		value: 'NingBo',
+		sub: [{
+			name: '海曙区',
+			value: 'HaiShuQu'
+		},{
+			name: '江北区',
+			value: 'JiangBeiQu'
+		},{
+			name: '北仑区',
+			value: 'BeiLunQu'
+		},{
+			name: '镇海区',
+			value: 'ZhenHaiQu'
+		},{
+			name: '鄞州区',
+			value: 'YinZhouQu'
+		},{
+			name: '奉化区',
+			value: 'FengHuaQu'
+		}]
+	},{
+		name: '金华',
+		value: 'JinHua',
+		sub: [{
+			name: '婺城区',
+			value: 'WuChengQu'
+		},{
+			name: '金东区',
+			value: 'JinDongQu'
+		}]
+	}]
+}];
